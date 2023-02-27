0acc22df8ca3f730122247c4e42abc81dfb702a4
Ramya Krishna <cheedellaramya@gmail.com>
2023-02-27 14:15:42 +0530

    initial-commit

---
+++ src/Components/Footer.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <div className='footer-container'>
+        <div className='footer-items'>
+            <i className="fa-brands fa-twitter footer-icons"></i>
+            <i className="fa-brands fa-facebook footer-icons"></i>
+            <i className="fa-brands fa-instagram footer-icons"></i>
+            <i className="fa-brands fa-github footer-icons"></i>
+        </div>
+    </div>
+  )
+}
+
+export default Footer
